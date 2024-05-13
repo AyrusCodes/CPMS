6eb6c0a45508cc591e5a4f1ada2bc65122a5a6a5
AyrusCodes <168162209+AyrusCodes@users.noreply.github.com>
2024-05-13 22:44:26 +0530

    Add files via upload

--- cpms/urls.py
+++ cpms/urls.py
@@ -25,10 +25,18 @@ urlpatterns = [
     path('student_login/student_reg/', user_views.student_reg, name='student_reg'),
     path('student_login/student_reg/studreg_details', user_views.studreg_details, name='studreg_details'),
     path('dashboard/', user_views.stud_dashboard, name='stud_dashboard'),
+    path('dashboard/stud_apply_job', user_views.stud_apply_job, name='stud_apply_job'),
+    path('dashboard/stud_results', user_views.stud_results, name='stud_results'),
+    path('dashboard/logout', user_views.logout, name='logout'),
 
     path('company_login/comp_reg/register_company/', user_views.CompanyUploadForm.as_view()),
     path('company_login/comp_reg/', user_views.comp_reg, name='comp_reg'),
     path('company_login/comp_reg/compreg_details', user_views.compreg_details, name='compreg_details'),
+    path('cdashboard/', user_views.comp_dashboard, name='comp_dashboard'),
+    path('cdashboard/apply_post', user_views.apply_post, name='apply_post'),
+    path('cdashboard/add_job/', user_views.JobUploadForm.as_view()),
+    # path('cdashboard/', user_views.comp_dashboard, name='comp_dashboard'),
+
     path('', include('cpmsapp.urls')),
 
  
