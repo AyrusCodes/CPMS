4f2e47361e9cf713169f9eea10e8c787c248fda1
AyrusCodes <168162209+AyrusCodes@users.noreply.github.com>
2024-05-13 13:11:41 +0530

    Add files via upload

---
+++ cpms/urls.py
@@ -0,0 +1,35 @@
+"""
+URL configuration for cpms project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from users import views as user_views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('student_login/student_reg/register_student/', user_views.StudentUploadForm.as_view()),
+    path('student_login/student_reg/', user_views.student_reg, name='student_reg'),
+    path('student_login/student_reg/studreg_details', user_views.studreg_details, name='studreg_details'),
+    path('dashboard/', user_views.stud_dashboard, name='stud_dashboard'),
+
+    path('company_login/comp_reg/register_company/', user_views.CompanyUploadForm.as_view()),
+    path('company_login/comp_reg/', user_views.comp_reg, name='comp_reg'),
+    path('company_login/comp_reg/compreg_details', user_views.compreg_details, name='compreg_details'),
+    path('', include('cpmsapp.urls')),
+
+ 
+]
